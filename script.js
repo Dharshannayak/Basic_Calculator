0f8e0e86b0e96ff71faf9dc48411159cfb0521d5
Dharshannayak <dharshannayak1@gmail.com>
2024-05-09 19:07:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let displayValue = '';
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+function del() {
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+function calculate() {
+    try {
+        const result = eval(displayValue);
+        if (!isFinite(result)) {
+            throw new Error('Math Error');
+        }
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+
